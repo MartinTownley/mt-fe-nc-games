abf2058cbff58508c924b3e712229d255709026e
martin <martinrtownley@gmail.com>
2023-03-15 16:19:45 +0000

    statically render single review page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
 import ReviewsList from "./components/ReviewsList";
+import SingleReview from "./components/SingleReview";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Nav />
-      <ReviewsList />
+      <Routes>
+        <Route path="/" element={<ReviewsList />} />
+        <Route path="/reviews/:review_id" element={<SingleReview />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/SingleReview.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SingleReview = () => {
+  return (
+    <main>
+      <h3>Single Review</h3>
+    </main>
+  );
+};
+
+export default SingleReview;
