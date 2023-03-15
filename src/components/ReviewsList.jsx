72133fb843ef9cfe61a1f381ffd53abd361616c3
martin <martinrtownley@gmail.com>
2023-03-15 15:57:13 +0000

    add loading message to ReviewsList component

--- src/components/ReviewsList.jsx
+++ src/components/ReviewsList.jsx
@@ -5,38 +5,48 @@ import { getReviews } from "../utils/api";
 
 const ReviewsList = () => {
   const [reviews, setReviews] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    setIsLoading(true);
     getReviews().then((reviewData) => {
-      console.log(reviewData, "<< reviewData");
-      const { reviews } = reviewData;
-      //console.log(reviews, "<< reviews");
       setReviews(reviewData);
+      setIsLoading(false);
     });
   }, []);
 
   return (
     <main className="items-list">
-      <h3>ReviewsList</h3>
-      {/* <ReviewCard /> */}
-      <ul>
-        <li>
-          {reviews.map(
-            ({ review_id, title, owner, created_at, votes, comment_count }) => {
-              return (
-                <ReviewCard
-                  key={review_id}
-                  title={title}
-                  owner={owner}
-                  created_at={created_at}
-                  votes={votes}
-                  comment_count={comment_count}
-                ></ReviewCard>
-              );
-            }
-          )}
-        </li>
-      </ul>
+      <h3>Reviews</h3>
+      {isLoading ? (
+        <p> Loading Reviews... </p>
+      ) : (
+        <ul>
+          <li>
+            {reviews.map(
+              ({
+                review_id,
+                title,
+                owner,
+                created_at,
+                votes,
+                comment_count,
+              }) => {
+                return (
+                  <ReviewCard
+                    key={review_id}
+                    title={title}
+                    owner={owner}
+                    created_at={created_at}
+                    votes={votes}
+                    comment_count={comment_count}
+                  ></ReviewCard>
+                );
+              }
+            )}
+          </li>
+        </ul>
+      )}
     </main>
   );
 };
